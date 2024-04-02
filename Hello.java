da8054103e546071c2e3124e820b94286a90354a
Nan <2429894028@qq.com>
2024-04-02 18:37:47 +0800

    测试Idea，添加了Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName: Hello
+ * @Package: PACKAGE_NAME
+ * @Description:
+ * @Author 菅圣楠
+ * @Create 2024-04-02 18:36 周二
+ * @Version 1.0
+ * @ProjectName: IntelliJ IDEA
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
